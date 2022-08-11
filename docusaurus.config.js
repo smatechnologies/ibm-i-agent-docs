43a56394c9c4ef796ff6217d03ad82d1f1f97757
rweesner <rweesner@smatechnologies.com>
2022-08-11 16:40:54 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,6 +42,7 @@ module.exports = {
             versions: {
             current: {
               label: '21.1',
+              path: ''
             },
             '18.1': {
               label: '18.1',
