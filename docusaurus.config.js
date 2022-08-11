bd84d01a38cc1de37028b417d3c5d4fb79c2c495
rweesner <rweesner@smatechnologies.com>
2022-08-11 15:03:53 -0500

    Revert "update-add-versioning"
    This reverts commit b2eb949c1c6fde8715419c237d012f6e35a0fec7.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,13 +17,6 @@ module.exports = {
         src: 'img/logo.svg',
         href: 'https://help.smatechnologies.com',
       },
-      items: [
-        {
-          type: 'docsVersionDropdown',
-          position: 'right',
-
-        },
-      ],
     },
     footer: {
       style: 'dark',
@@ -39,16 +32,6 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
             'https://github.com/smatechnologies/ibm-i-agent-docs/blob/develop',
-            versions: {
-              'current': {
-                label: '21.1',
-              },
-              '18.1': {
-                label: '18.1',
-                path: 'v18.1',
-                banner: 'none',
-              },
-            }
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
